e791eae563c4765849245348324f0dd9382b6445
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2024-01-17 11:37:29 -0500

    got all helper functions completed and exported

--- server/db/helpers/MVs.js
+++ server/db/helpers/MVs.js
@@ -2,9 +2,29 @@ const client = require("../client");
 const util = require('../util');
 
 const getAllMv = async () => {
-    "#"
+    try {
+        const { rows } 
+            = await client.query(`
+            SELECT * FROM musicVids;
+            `)
+            return rows;
+    } catch(error) {
+        console.error(error);
+    }
 };
 
-async function getMvById() {
-    "#"
-}
+async function getMvById(mv_id) {
+    try {
+        const { rows: [musicVids] }
+            = await client.query(`
+            SELECT *
+            FROM musicVids
+            WHERE "mv_id" =${mv_id};
+            `)
+            return musicVids;
+    } catch(error) {
+        console.error(error);
+    }
+};
+
+module.exports = { getAllMv, getMvById }
--- server/db/helpers/carats.js
+++ server/db/helpers/carats.js
@@ -1,2 +1,5 @@
 const client = require("../client");
 const util = require('../util');
+
+// THIS FILE IS RELATED TO MY STRETCH GOAL-- WILL BE COMPLETING AT A LATER DATE
+
