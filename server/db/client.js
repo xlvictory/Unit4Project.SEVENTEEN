8ca01ab6def6fcf2fe0c63eddbee0432e0959adc
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2024-01-30 01:29:35 -0500

    internal db link update

--- server/db/client.js
+++ server/db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 const dbName = "svt_right_here";
-const client = new Client(`postgres://localhost:54321/${dbName}`);
+const client = new Client(`postgres://svt_right_here_user:rPITKjnsEbwcIYUSOLVtDQcUrHaZDfLL@dpg-cms95uqcn0vc73bevpeg-a/svt_right_here`);
 
 
 module.exports = client;
